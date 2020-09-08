51a5edb88b981dc045a7b656391c905be47fbdeb
Jordy Navarrete <jordydavid1@gmail.com>
2020-09-08 10:16:11 -0500

    docs agregado

---
+++ docs/precache-manifest.30a5ba0aedf70d258b364408142effbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33900f505e498bf136d968806468525a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b923b97eb73ee4e412d",
+    "url": "/static/css/main.7ddfa072.chunk.css"
+  },
+  {
+    "revision": "f9234fcf84c080de371a",
+    "url": "/static/js/2.982de7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.982de7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b923b97eb73ee4e412d",
+    "url": "/static/js/main.da583699.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
